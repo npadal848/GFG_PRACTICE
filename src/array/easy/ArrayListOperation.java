78049a17df3464115541b2d8d622c5a2cbb8523c
Nageswar Padal <npadal848@gmail.com>
2021-04-03 17:09:04 +0530

    Added initial commit

---
+++ src/array/easy/ArrayListOperation.java
@@ -0,0 +1,24 @@
+package array.easy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayListOperation {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> clist=new ArrayList<>();
+		clist.add(2);
+		clist.add(2);
+		clist.add(3);
+		clist.add(4);
+		clist.add(3);
+		
+
+		System.out.println(Collections.binarySearch(clist, 2));
+		System.out.println(clist);
+	}
+	
+	
+}
+
