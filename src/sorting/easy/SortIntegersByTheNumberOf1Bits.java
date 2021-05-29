7a500abf8ccf09bea359425f6d9909605834b335
Nageswar Padal <npadal848@gmail.com>
2021-05-29 23:33:07 +0530

    Added sorting practice solutions

---
+++ src/sorting/easy/SortIntegersByTheNumberOf1Bits.java
@@ -0,0 +1,14 @@
+package sorting.easy;
+
+public class SortIntegersByTheNumberOf1Bits {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	static int[] sort(int[] arr) {
+		int[] res=new int[arr.length];
+		
+		return res;
+	}
+}
